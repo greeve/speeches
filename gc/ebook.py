4dbd1e166719e6d836d359e5157629f73f71f26b
greeve <greg_reeve@byu.edu>
2017-07-07 00:52:51 -0600

    Start constructing the epub generation

---
+++ gc/ebook.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""
+Construct an epub of talks by apostles for a given conference.
+"""
+
+__author__ = "Greg Reeve"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+MIMETYPE_PATH = 'epub/mimetype'
+CONTAINER_PATH = 'epub/META-INF/container.xml'
+
+PACKAGE_PATH = 'epub/EPUB/package.opf'
+NAV_PATH = 'epub/EPUB/xhtml/nav.xhtml'
+TITLE_PATH = 'epub/EPUB/xhtml/title.xhtml'
+LANG_PATH = 'epub/EPUB/xhtml/{lang}.xhtml'
+LANG_DIR = 'epub/EPUB/xhtml/{lang}/'
+TALK_PATH = LANG_DIR + 'talk.xhtml'
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
